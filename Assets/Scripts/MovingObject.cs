0a7e2b8007559624f2b70dc90a5d86f090a4db8e
AIE\s210491 <s210491@students.aie.edu.au>
2021-05-03 10:55:38 +0930

    Initial Commit-Buildings and Paths added prior to creation of repository

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    [Tooltip("The position to move this object to. The object will move back to it's starting position, if you want.")]
+    public Transform targetPosition;
+	[Tooltip("How fast the object moves, in units per second.")]
+    public float moveSpeed;
+	[Tooltip("Should the object start moving on scene start?")]
+    public bool moveOnStart;
+	[Tooltip("Should the object move back and forth?")]
+	public bool	loop;
+    private Vector3 startPostion;
+    private bool moving, moveToTarget = true;
+
+    private void Start()
+    {
+        startPostion = transform.position;
+        if (targetPosition.parent == transform)
+            targetPosition.parent = null;
+        if (moveOnStart)
+            Move();
+    }
+    private void Update()
+    {
+        if (!moving) return;
+        Vector3 targetPos = moveToTarget ? targetPosition.position : startPostion;
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * moveSpeed);
+        if (transform.position == targetPos)
+        {
+            if (loop)
+            {
+                moveToTarget = !moveToTarget;
+            }
+            else
+            {
+                moving = false;
+            }
+        }
+            
+    }
+
+    public void Move()
+    {
+        if (moving) return;
+        moveToTarget = transform.position == startPostion ? true : false;
+        moving = true;
+    }
+
+}
---
+++ Assets/Scripts/PlayerTeleporter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleporter : MonoBehaviour
+{
+	[Tooltip("The gameobject the player should teleport too.")]
+    public Transform teleportToLocation;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.layer == 9 || collider.gameObject.tag == "Player")
+        {
+            CharacterController cc = collider.GetComponent<CharacterController>();
+
+            cc.enabled = false;
+            collider.transform.position = teleportToLocation.position;
+            cc.enabled = true;
+        }
+    }
+}
